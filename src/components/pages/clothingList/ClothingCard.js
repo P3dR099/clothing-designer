fcec710decc084555fb30347f806ad3dcd26b14e
Pedro Conde <condepedro968@gmail.com>
2020-10-19 05:22:39 +0200

    add responsive cards

--- src/components/pages/clothingList/ClothingCard.js
+++ src/components/pages/clothingList/ClothingCard.js
@@ -12,22 +12,19 @@ import './clothingCard.css'
 
 const ClothingCard = ({ _id, title, imageUrl, loggedInUser, user, color, logo }) => {
 
-
     return (
-        <Col md={4}>
-            <Card className="coaster-card">
-                <Col xs="auto" md="auto" className="page" id="shirtCard" style={{ 'backgroundColor': color }}>
-                    <div className='img-container'>
-                        <div id="drawingArea" >
-                            <Canvas id="tcanvas" className="img-responsive" width="200px" height="400px" />
-                        </div>
-                        <div>
-                            <Card.Img id="tshirtFacing" src={imageUrl} alt="camiseta de manga corta">
-                                {/* <img src={logo}></img> */}
-                            </Card.Img>
-                        </div>
+        <Col md={3} sm xs={4} style={{ margin: '20px' }}>
+            <Card style={{ alignItems: 'center' }}>
+                <div className='img-container' style={{ 'backgroundColor': color }} >
+                    <div id="drawingArea" >
+                        <Canvas id="tcanvas" className="img-responsive" width="200px" height="400px" />
                     </div>
-                </Col >
+                    <div>
+                        <Card.Img id="tshirtFacing" src={imageUrl} alt="camiseta de manga corta">
+                        </Card.Img>
+                    </div>
+                </div>
+
                 <Card.Body>
                     <h4>{title}</h4>
 
